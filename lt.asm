800510a503e5b7612ae15c30588063e343631cf7
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-06-11 20:41:11 +0700

    edit lession 1

--- lt.asm
+++ lt.asm
@@ -1,41 +1,46 @@
 INCLUDE lib1.asm
 .MODEL small
-.STACK 100h ; Mô hình bộ nhớ dành cho chương trình dạng small
-.DATA ; Dành một vùng nhớ để cấp phát biến
-	m1 db 13, 10, 'Hay vao a: $'
-	m2 db 13, 10, 'Hay vao n: $'
-	ctlf db 13, 10, '$'
+.STACK 100h 				; Mô hình bộ nhớ dành cho chương trình dạng small
+.DATA 						; Dành một vùng nhớ để cấp phát biến
+	m1 	db 13, 10, 'CAC BAI TAP NGON NGU ASSEMBLY THUAN TUY'
+		db 13, 10, '---------------------------------------'
+		db 13, 10, 13, 10, 'Bai 1: Tinh a luy thua n (a la so nguyen va n la so nguyen duong'
+		db 13, 10, 13, 10, '---------------------------------------'
+		db 13, 10, 13, 10, '------------------- CHUONG TRINH --------------------'
+		db 13, 10, 'Hay vao a: $'
+	m2 db 13, 'Hay vao n: $'
+	ctlf db 13, '$'
 	m3 db ' luy thua $'
 	m4 db ' la: $'
 	m5 db 13, 10, 'Co tiep tuc chuong trinh (c/k) khong? $'
 .CODE
 PS:
-	mov ax, @data ; Đưa phần địa chỉ segment vùng nhó dành cho dữ liệu
-	mov ds, ax ; vào ds (chỉ có khi có .data, có khai báo biến) 
+	mov ax, @data 			; Đưa phần địa chỉ segment vùng nhó dành cho dữ liệu
+	mov ds, ax 				; vào ds (chỉ có khi có .data, có khai báo biến) 
 	clrscr
 	HienString m1 
-	call VAO_SO_N ; Nhận giá trị a
-	mov bx, ax ; bx = a
-	HienString m2 ; Hiện thông báo m2 
-	call VAO_SO_N ; Nhận giá trị n 
-	mov cx, ax ; cx = n 
-	HienString ctlf ; Quay đầu dòng và xuống hàng 
-	mov ax, bx ; ax = a
-	call HIEN_SO_N ; Hiện giá trị a lên màn hình
+	call VAO_SO_N 			; Nhận giá trị a
+	mov bx, ax 				; bx = a
+	HienString m2 			; Hiện thông báo m2 
+	call VAO_SO_N 			; Nhận giá trị n 
+	mov cx, ax 				; cx = n 
+	HienString ctlf 		; Quay đầu dòng và xuống hàng 
+	mov ax, bx 				; ax = a
+	call HIEN_SO_N 			; Hiện giá trị a lên màn hình
 	HienString m3 
-	mov ax, cx ; ax = n
-	call HIEN_SO_N ; Hiện giá trị n lên màn hình
+	mov ax, cx 				; ax = n
+	call HIEN_SO_N 			; Hiện giá trị n lên màn hình
 	HienString m4
-	mov ax, 1 ; Gán ax = 1
-	and cx, cx ; Liệu giá trị n (cx = n) có bằng 0?
-	jz HIEN ; Nếu bằng 0 thì nhảy đến nhãn HIEN 
+	mov ax, 1 				; Gán ax = 1
+	and cx, cx 				; Liệu giá trị n (cx = n) có bằng 0?
+	jz HIEN 				; Nếu bằng 0 thì nhảy đến nhãn HIEN 
 	
-	LAP: ; còn không thì thực hiện vòng lặp tính a lũy thừa n 
+	LAP: 					; còn không thì thực hiện vòng lặp tính a lũy thừa n 
 		mul bx ; ax = ax * bx 
 		loop LAP
 	
 	HIEN:
-		call HIEN_SO_N ; Hiện giá trị a lũy thừa n (giá trị có trong ax)
+		call HIEN_SO_N 		; Hiện giá trị a lũy thừa n (giá trị có trong ax)
 		HienString m5 
 		mov ah, 1 
 		int 21h 
