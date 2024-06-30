79a527d16d1d4fb93ab8125ff5531c3690354dbc
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-06-30 16:26:01 +0700

    edit lession 4 (dos pg)

--- cdkmh.asm
+++ cdkmh.asm
@@ -2,13 +2,23 @@ INCLUDE lib1.asm
 .MODEL small 
 .STACK 100h
 .DATA
-    m1 db 13, 10, 'Loai card dieu khien man hinh ma may tinh co la: $'
+    m1	db 13, 10, '	LAP TRINH TREN MOI TRUONG DOS'
+		db 13, 10, '	-----------------------------'
+		db 13, 10, 13, 10, '	Bai 4: Hay viet chuong trinh co biet may tinh ban dang dung'
+		db 13, 10, 'co card dieu khien man hinh loai gi (mono hay color)?'
+		db 13, 10, 13, 10, '	-----------------------------'
+		db 13, 10, 13, 10, '	--------------- CHUONG TRINH --------------'
+		db 13, 10, '	Loai card dieu khien man hinh ma may tinh co la: $'
     color db 'Color $'
     mono db 'Mono $'
+	tieptuc db 13, 10, '	-----------------------------'
+			db 13, 10, '	Co tiep tuc chuong trinh khong(c/k)? $'
 .CODE
 PS:
         mov ax, @data 
         mov ds, ax 
+	
+	L_CARD0:
         clrscr
         HienString m1
         int 11h ; Ngắt hệ thống thực hiện việc đưa nội dung ô nhớ 0:410h -> al
@@ -20,10 +30,16 @@ PS:
 
     L1:
         HienString Color
+		
+	CONTINUE:
+		HienString tieptuc 
+		mov ah, 1
+		int 21h 
+		cmp al, 'c'
+		jne Exit
+		jmp L_CARD0
 
     Exit: 
-        mov ah, 1
-        int 21h
         mov ah, 4ch 
         int 21h
 END PS
