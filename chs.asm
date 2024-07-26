7d2a52484edae037305b9b947d50e1e0219a5aef
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-07-26 16:35:24 +0700

    edit date.asm, chs.asm

--- chs.asm
+++ chs.asm
@@ -4,52 +4,68 @@ include lib1.asm
 .data                       ; Dành một vùng nhớ để cấp phát cho biến (chỉ có khi chương trình có khai báo biến)
     m1 db 10, 13, 'Hay vao so bi chia: $'
     m2 db 10, 13, 'Hay vao so chia: $'
+	nhaplai db 13, 10, 'Hay vao lai so chia: $'
     m3 db 10, 13, 'Thuong la: $'
+	khac0 db 13, 10, 'So chia phai khac 0! $'
     dautru db '-$'
     daucham db '.$'
     m4 db 10, 13, 'Co tiep tuc (c/k)? $'
+	buff 	dw 8				; Không nên khỏi tạo ban đầu (dấu ?)
+			dw ?
+			dw 8 dup(?)
 .code 
 PS:
-    mov ax, @data           ; Đưa phần địa chỉ segment vào vùng nhớ
-    mov ds, ax              ; dành cho dữ liệu vào ds ( chỉ có khi có .data, có khai báo biến)
-    clrscr                  ; Xoá màn hình
-    HienString m1
-    call VAO_SO_N           ; Nhận giá trị số bị chia
-    mov bx, ax              ; bx = số bị chia
-    HienString m2           ; Hiện thông báo m2 ('Hay vao so chia')
-    call VAO_SO_N           ; Nhận giá trị số chia (ax = số chia)
-    xchg ax, bx             ; Đổi chéo (ax = số bị chia, bx = số chia)
-    HienString m3           ; Hiện thông báo m3 ('Thuong la: ')
-    and ax, ax              ; Dựng cờ dấu của số bị chia (dấu thương cùng dấu số bị chia)
-    jns CHIA1               ; Nếu dấu số bị chia là dương thì nhảy đến nhãn CHIA1
-    HienString dautru       ; còn nếu là số bị âm thì hiệ dấu '-' lên màn hình (dấu âm)
-    neg ax                  ; Đổi dấu số bị chia
+		mov ax, @data           ; Đưa phần địa chỉ segment vào vùng nhớ
+		mov ds, ax				; dành cho dữ liệu vào ds ( chỉ có khi có .data, có khai báo biến)
+	L_CT0:
+		clrscr                  ; Xoá màn hình
+		HienString m1
+		call VAO_SO_N  
+		mov bx, ax              ; bx = số bị chia
+		HienString m2           ; Hiện thông báo m2 ('Hay vao so chia')
+	L_CT1:
+		call VAO_SO_N           ; Nhận giá trị số chia (ax = số chia)
+		cmp ax, 0
+		je L_CT2
+		xchg ax, bx             ; Đổi chéo (ax = số bị chia, bx = số chia)
+		HienString m3           ; Hiện thông báo m3 ('Thuong la: ')
+		and ax, ax              ; Dựng cờ dấu của số bị chia (dấu thương cùng dấu số bị chia)
+		jns CHIA1               ; Nếu dấu số bị chia là dương thì nhảy đến nhãn CHIA1
+		HienString dautru       ; còn nếu là số bị âm thì hiệ dấu '-' lên màn hình (dấu âm)
+		neg ax                  ; Đổi dấu số bị chia
     CHIA1:
-        xor dx, dx          ; dx = 0
-        div bx              ; dx : ax chia cho bx (ax = thương còn dx dư)
-        call HIEN_SO_N      ; Hiện giá trị của thương lên màn hình
-        and dx, dx          ; Dựng cờ của phần dư (Z = 1 thì dư = 0, còn Z = 0 thì dư # 0)
-        jz KT               ; Dư = 0 thì nhảy đến kết thúc quá trình chia
-        HienString daucham  ; còn không thì hiện dấu chấm ('.') và tiếp tục chia
-        mov cx, 2           ; số chữ số sau số thập phân
-        mov si, 10          ; si = 10
+        xor dx, dx          	; dx = 0
+        div bx              	; dx : ax chia cho bx (ax = thương còn dx dư)
+        call HIEN_SO_N      	; Hiện giá trị của thương lên màn hình
+        and dx, dx          	; Dựng cờ của phần dư (Z = 1 thì dư = 0, còn Z = 0 thì dư # 0)
+        jz KT               	; Dư = 0 thì nhảy đến kết thúc quá trình chia
+        HienString daucham  	; còn không thì hiện dấu chấm ('.') và tiếp tục chia
+        mov cx, 2           	; số chữ số sau số thập phân
+        mov si, 10          	; si = 10
     CHIA2:
-        mov ax, dx          ; Đưa phần dư vào ax
-        mul si              ; Nhân phần dư cho 10
-        div bx              ; dx:ax chia cho bx
-        call HIEN_SO_N      ; Hiện giá trị của thương lên màn hình
-        and dx, dx          ; Dựng cờ của phần dư (Z = 1 thì dư 0, còn Z = 0 thì dư # 0)
-        jz KT               ; Phần dư bằng 0 thì nhảy đến kết thúc quá trình chia
+        mov ax, dx          	; Đưa phần dư vào ax
+        mul si              	; Nhân phần dư cho 10
+        div bx              	; dx:ax chia cho bx
+        call HIEN_SO_N      	; Hiện giá trị của thương lên màn hình
+        and dx, dx          	; Dựng cờ của phần dư (Z = 1 thì dư 0, còn Z = 0 thì dư # 0)
+        jz KT               	; Phần dư bằng 0 thì nhảy đến kết thúc quá trình chia
         loop CHIA2
+		jmp KT
+		
+	L_CT2:
+		HienString khac0
+		HienString nhaplai
+		jmp L_CT1
+		
     KT:
-        HienString m4       ; Hiện thông báo m4 'Co tiep tuc chuong trinh(c/k)?'
-        mov ah, 1           ; Chờ nhận một ký tự từ bàn phím
+        HienString m4       	; Hiện thông báo m4 'Co tiep tuc chuong trinh(c/k)?'
+        mov ah, 1           	; Chờ nhận một ký tự từ bàn phím
         int 21h
-        cmp al, 'c'         ; Ký tự vừa nhận có phải là ký tự 'c'
-        jne Exit            ; Nếu không phải thì nhảy đến nhãn Exit (về DOS)
-        jmp PS              ; còn không thì quay về đầu (bắt đầu lại chương trình)
+        cmp al, 'c'         	; Ký tự vừa nhận có phải là ký tự 'c'
+        jne Exit            	; Nếu không phải thì nhảy đến nhãn Exit (về DOS)
+        jmp PS              	; còn không thì quay về đầu (bắt đầu lại chương trình)
     Exit:
-        mov ah, 4ch         ; Về DOS
+        mov ah, 4ch         	; Về DOS
         int 21h
-include lib2.asm
+include LIB5.asm
 end PS
--- date.asm
+++ date.asm
@@ -3,6 +3,13 @@ INCLUDE lib1.asm
 .STACK 100h
 .DATA
 	lt1 db 13, 10, 'Ngay, thang, nam hien tai cua may tinh la: $'
+	mon db 13, 10, 'Thu 2$'
+	tue db 13, 10, 'Thu 3$'
+	wed db 13, 10, 'Thu 4$'
+	thu db 13, 10, 'Thu 5$'
+	fri db 13, 10, 'Thu 6$'
+	sat db 13, 10, 'Thu 7$'
+	sun db 13, 10, 'Chu nhat$'
 	hc db ':$'
 	dc db '.$'
 	tieptuc db 13, 10, 'Co tiep tuc chuong trinh khong(c/k)? $'
@@ -15,11 +22,55 @@ INCLUDE lib1.asm
 	L_LT0:
 		CLRSCR
 		HienString lt1
-		mov ah, 2ah ; Chức năng lấy giờ, phút, giây của máy tính 
+		mov ah, 2ah ; Chức năng lấy thứ, ngày, tháng, năm của máy tính 
 		int 21h
 		mov al, al ; Đưa thứ từ al -> al
 		xor ah, ah ; Chuyển ah = 0, al = 0, 1, 2, 3, 4, 5, 6
-		call HIEN_SO_N ; Hiện thứ 
+		cmp al, 0
+		je PRINT_SUN ; Nhảy nếu [Đích] = [Nguồn]; Tương tự như jz; (JF=0)
+		cmp al, 1
+		je PRINT_MON
+		cmp al, 2
+		je PRINT_TUE
+		cmp al, 3
+		je PRINT_WED
+		cmp al, 4
+		je PRINT_THU
+		cmp al, 5
+		je PRINT_FRI
+		cmp al, 6
+		je PRINT_SAT
+		;call HIEN_SO_N ; Hiện thứ 
+		
+	PRINT_MON:
+		HienString mon
+		jmp L_LT1
+		
+	PRINT_TUE:
+		HienString tue
+		jmp L_LT1
+		
+	PRINT_WED:
+		HienString wed
+		jmp L_LT1
+		
+	PRINT_THU:
+		HienString thu
+		jmp L_LT1
+	
+	PRINT_FRI:
+		HienString fri
+		jmp L_LT1
+	
+	PRINT_SAT:
+		HienString sat
+		jmp L_LT1
+		
+	PRINT_SUN:
+		HienString sun
+		jmp L_LT1
+	
+	L_LT1:
 		HienString dc
 		mov al, dl ; Đưa ngày từ dl -> al 
 		xor ah, ah ; ah = 0
@@ -31,6 +82,7 @@ INCLUDE lib1.asm
 		HienString dc
 		mov ax, cx ; Đưa năm từ cx -> al
 		call HIEN_SO_N ; Hiện năm
+		jmp CONTINUE
 		
 	CONTINUE:
 		HienString tieptuc
